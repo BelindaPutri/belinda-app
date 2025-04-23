216b761e0f97c86563bf3fcbd13de65f2b48afb1
BelindaPutri <249belinda@gmail.com>
2025-04-23 14:55:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("bey new app")
 st.write(
     "something happens and I'm head over heels[docs.streamlit.io](https://docs.streamlit.io/).")
+    "stecu stecu call me 911"
