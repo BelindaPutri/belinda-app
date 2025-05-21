44ddfb9ef7539ce77a8d96a51c429c5f16a9a830
BelindaPutri <249belinda@gmail.com>
2025-05-21 14:47:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("bey new app")
 st.write(
     "stecu stecu something happens and I'm head over heels[docs.streamlit.io](https://docs.streamlit.io/).")
-st.image("IMG-20250509-WA0298(1).jpg")
+st.image("IMG-20250509-WA0298(1).jpg", width=200)
