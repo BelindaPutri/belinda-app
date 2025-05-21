9925163129b5fff57d2288fe5934b914245eb926
BelindaPutri <249belinda@gmail.com>
2025-05-21 14:41:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("bey new app")
 st.write(
     "stecu stecu something happens and I'm head over heels[docs.streamlit.io](https://docs.streamlit.io/).")
+( https://github.com/BelindaPutri/belinda-app/tree/main)
