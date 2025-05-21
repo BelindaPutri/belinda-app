9c4fa7e5dced5d02939615ad82d2faa5d4b5b3c2
BelindaPutri <249belinda@gmail.com>
2025-05-21 15:13:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ st.write(
     "stecu stecu something happens and I'm head over")
 st.image("IMG-20250509-WA0298(1).jpg",width=200)
 
-st.title("Aplikasi Mewah")
-st.header("Aplikasi untuk Mengecek Tabungan")
+st. title("Aplikasi Mewah")
+st. header("Aplikasi untuk Mengecek Tabungan")
 angka = st.number_input("Tulislah sebuah nominal:", value=0, step=1)
+
 if (angka % 2) == 0:
-    st.write(f"{angka} adalah Bilangan Genap")
+    st.write(f"{angka} Tabungan Genap")
 else:
-    st.write(f"{angka} adalah Bilangan Ganjil")
+    st.write(f"{angka} Tabungan Ganjil")
